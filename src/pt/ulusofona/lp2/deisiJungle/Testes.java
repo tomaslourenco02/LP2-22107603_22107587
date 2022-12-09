1f64b9c5191e362b6e4547b1cec56077e64699c6
tomaslourenco02 <tomaslourenco02@gmail.com>
2022-12-09 11:33:12 +0000

    atualizacoes gameManager

--- src/pt/ulusofona/lp2/deisiJungle/Testes.java
+++ src/pt/ulusofona/lp2/deisiJungle/Testes.java
@@ -7,7 +7,8 @@ import java.util.Arrays;
 import static org.junit.Assert.assertTrue;
 
 public class Testes {
-    @org.junit.Test
+
+    /*@org.junit.Test
 
     public void testJungle(){
         GameManager gamemaneger = new GameManager();
@@ -48,43 +49,42 @@ public class Testes {
         System.out.println(Arrays.toString(gamemaneger.getCurrentPlayerInfo()));
         System.out.println("Quem ganhou "  + Arrays.toString(gamemaneger.getWinnerInfo()) + "\n");
 
-        /*
+
         System.out.println("1 jogada \n");
 
         System.out.println("quem está a ganhar:");
         System.out.println(gamemaneger.winnerPlayer() );
-        System.out.println(Arrays.toString(gamemaneger.getWinnerInfo())+ "\n"); */
+        System.out.println(Arrays.toString(gamemaneger.getWinnerInfo())+ "\n");
 
-        System.out.println(gamemaneger.moveCurrentPlayer(6, false));
+            System.out.println(gamemaneger.moveCurrentPlayer(6,false));
         System.out.println(Arrays.toString(gamemaneger.getCurrentPlayerInfo()));
-        System.out.println(gamemaneger.moveCurrentPlayer(6, false));
+        System.out.println(gamemaneger.moveCurrentPlayer(6,false));
         System.out.println(Arrays.toString(gamemaneger.getCurrentPlayerInfo()));
-        System.out.println(gamemaneger.moveCurrentPlayer(6, false));
+        System.out.println(gamemaneger.moveCurrentPlayer(6,false));
         System.out.println(Arrays.toString(gamemaneger.getCurrentPlayerInfo()));
-        System.out.println(gamemaneger.moveCurrentPlayer(6, false));
+        System.out.println(gamemaneger.moveCurrentPlayer(6,false));
         System.out.println(Arrays.toString(gamemaneger.getCurrentPlayerInfo()));
 
-        System.out.println(gamemaneger.moveCurrentPlayer(6, false));
+        System.out.println(gamemaneger.moveCurrentPlayer(6,false));
 
 
-        System.out.println("Quem ganhou" + Arrays.toString(gamemaneger.getWinnerInfo()) + "\n");
+        System.out.println("Quem ganhou"+Arrays.toString(gamemaneger.getWinnerInfo())+"\n");*/
 
+}
 
-    }
-
-    @org.junit.Test
-    public void testGetSquareInfo(){
+    /*@org.junit.Test
+    public void testGetSquareInfo() {
         GameManager gamemaneger = new GameManager();
         String[] jogador1 = {"4", "Joao", "E"};
         String[] jogador2 = {"5", "Miguel", "L"};
         String[] jogador3 = {"89", "José", "Z"};
         String[] jogador4 = {"34", "Ana", "P"};
 
-        String[][] jogadores = {jogador1, jogador2,jogador3,jogador4};
+        String[][] jogadores = {jogador1, jogador2, jogador3, jogador4};
 
-        gamemaneger.createInitialJungle(10,2, jogadores);
+        gamemaneger.createInitialJungle(10, 2, jogadores);
 
         System.out.println(Arrays.toString(gamemaneger.getSquareInfo(1)));
         System.out.println(Arrays.toString(gamemaneger.getPlayerIds(1)));
     }
-}
+*/
