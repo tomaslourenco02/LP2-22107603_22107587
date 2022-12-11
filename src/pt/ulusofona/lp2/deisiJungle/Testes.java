52773ba44d4280160500072af9589eee56f79398
joanagoncalves13 <jjoanagoncalves.13@gmail.com>
2022-12-11 21:16:37 +0000

    caso a food esteja na casa de partida (1)

--- src/pt/ulusofona/lp2/deisiJungle/Testes.java
+++ src/pt/ulusofona/lp2/deisiJungle/Testes.java
@@ -32,9 +32,9 @@ public class Testes {
 
         String[][] jogadores = {jogador1, jogador2,jogador3,jogador4};
 
-        String[] comida1 = {"b", "w"};
+        String[] comida1 = {"b", String.valueOf(6)};
         String[] comida2 = {"b", String.valueOf(7)};
-        String[] comida3 = {"a", String.valueOf(1)};
+        String[] comida3 = {"a", String.valueOf(4)};
 
         String[][] comida = {comida1, comida2,comida3};
 
